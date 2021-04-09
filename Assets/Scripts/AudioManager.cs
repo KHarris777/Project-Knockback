652f5d5932774bcb12c993406de47ac9b2f99054
Lurlichs <loganurlichs@gmail.com>
2021-04-09 20:35:28 +1000

    Changes from class Plus temp audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Sound[] sounds;
+
+    void Start()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.priority = s.priority;
+        }   
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+   
+}
